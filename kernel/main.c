d527c80a962a4a040df842b77e284e085dc14304
gzqccnu <gzqccnu@gmail.com>
2025-11-22 07:50:50 +0000

    modify kernel main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,17 +1,32 @@
-// main.c - 内核主函数
+// main.c - kernel main
 
-#include "types.h"
-#include "uart.h"     // 声明的 printk 函数
-#include "color.h"
+#include "include/log.h"
+#include "mem/kmem.h"  // kernel memory manager interface
+#include "trap/trap.h" // interrupt and exception handling
+#include "uart/uart.h" // declarations for uart_init and printk
 
-// 内核主函数
+extern char _heap_start[]; // from linker.ld define where the heap starts
+extern char _heap_end[];   // from linker.ld define where the heap ends
+
+// kernel main function
 int kmain() {
-    // 初始化控制台
-    uart_init();
-    // 测试printk函数
-    INFO("welcome to MiniOS");
-    // 进入主循环
-    while (1) {
-        // 内核主循环
-    }
+
+  trap_init();                   // trap/interrupt initialization
+  uart_init();                   // UART initialization for serial output
+  kinit(_heap_start, _heap_end); // initialize kernel memory manager
+  INFO("welcome to MiniOS!");
+
+  printk("free pages: %d\n", get_free_pages());
+  printk("total pages: %d\n", get_total_pages());
+  int *ptr = kalloc();
+  printk("Allocated page at address: 0x%x\n", ptr);
+  print_memory_stats();
+  printk("free pages: %d\n", get_free_pages());
+  printk("total pages: %d\n", get_total_pages());
+  kfree((void *)ptr);
+  printk("Freed page at address: 0x%x\n", ptr);
+  print_memory_stats();
+  printk("free pages: %d\n", get_free_pages());
+  printk("total pages: %d\n", get_total_pages());
+  return 0;
 }
