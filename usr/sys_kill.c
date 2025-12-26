e922ecf57c0b701cd97a9d641245709f17e3bc54
lrisguan <lrisguan@outlook.com>
2025-12-26 06:52:07 +0000

    usr/sys_kill.c: add kill syscall

---
+++ usr/sys_kill.c
@@ -0,0 +1,14 @@
+/*
+ * Lrix
+ * Copyright (C) 2025 lrisguan <lrisguan@outlook.com>
+ *
+ * This program is released under the terms of the GNU General Public License version 2(GPLv2).
+ * See https://opensource.org/licenses/GPL-2.0 for more information.
+ *
+ * Project homepage: https://github.com/lrisguan/Lrix
+ * Description: A scratch implemention of OS based on RISC-V
+ */
+
+#include "user.h"
+
+int sys_kill(int pid) { return (int)sys_call3(SYS_KILL, (uint64_t)pid, 0, 0); }
